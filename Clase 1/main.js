4322c0f58378544732594ee0f4c9e2b435e1696d
andreazapata <andreazapatareza@gmail.com>
2021-09-30 18:40:29 -0500

    Clase 1

---
+++ Clase 1/main.js
@@ -0,0 +1,5 @@
+let name = prompt("Ingresa tu nombre");
+let lastName = prompt("Ingresa tu apellido");
+const blank = " ";
+let welcome = "¡Bienvenide," + blank + name + blank + lastName + "!"
+alert(welcome);
