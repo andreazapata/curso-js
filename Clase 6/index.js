0306437511816902dc985cc5c747ba9fa691c1e3
andreazapata <andreazapatareza@gmail.com>
2021-10-19 18:14:07 -0500

    Clase 6: arrays

--- Clase 6/index.js
+++ Clase 6/index.js
@@ -1,14 +1,10 @@
-const productos = [{ id: 1,  producto: "Arroz", precio: 125 },
-                  {  id: 2,  producto: "Fideo", precio: 70 },
-                  {  id: 3,  producto: "Pan"  , precio: 50},
-                  {  id: 4,  producto: "Flan" , precio: 100}];
+const flores = [{ id: 1,  producto: "rosa", precio: 10 },
+                  {  id: 2,  producto: "peonia", precio: 50 },
+                  {  id: 3,  producto: "tulipan"  , precio: 20},
+                  {  id: 4,  producto: "gerbera" , precio: 15}];
 
-const buscarPan = productos.find(producto => producto.id === 3); 
-console.log(buscarPan);//{id: 3, producto: "Pan", precio: 50}
+const baratos = flores.filter(flores => flores.precio < 50); 
+console.log(baratos);
 
-const baratos = productos.filter(producto => producto.precio < 100); 
-console.log(baratos);//[{id: 2,producto:"Fideo",precio:70},{id:3,producto:"Pan",precio: 50}]
-
-const aumentos = productos.map(producto => producto.precio += 30);
-console.log(aumentos);
-//[155, 100, 80, 130] y el valor de cada producto cambio.
+const iva = flores.map(flores => flores.precio = flores.precio*1.16);
+console.log(iva);
